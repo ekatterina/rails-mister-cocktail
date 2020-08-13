b431c5dcf61cbde65c462d7cbfe01752432931bf
Ekaterina Tugarinova <kate.tugarinova@gmail.com>
2020-08-13 12:11:18 +0200

    commit2

---
+++ app/models/cocktail.rb
@@ -0,0 +1,5 @@
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, -> { distinct }, through: :doses
+  validates :name, presence: true, uniqueness: true
+end
